d208ca0f837f6b75f92f62e2b335deb91c9169a4
ismailproject <ismailahmmedroman@gmaiil.com>
2022-08-23 18:27:16 +0600

    selected Player setup conpleted

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const playerarray = [];
-let totalPalyersemty = document.getElementById('total-plyer');
+
 // selectedPlayers area
 function displayPlayer(selectedPlayers) {
 
@@ -7,13 +7,12 @@ function displayPlayer(selectedPlayers) {
     // console.log("selectedPlayers");
     const tablelist = document.getElementById('name-list')
     tablelist.innerHTML = '';
-    if (playerarray.length == 5) {
-        totalPalyersemty.innerHTML = '0';
+    // if (selectedPlayers.length >= 2) {
+    //     alert('hi');
+    //     return;
+    // } else {
 
-
-        alert('hi');
-        return location.reload();
-    }
+    // }
     for (let i = 0; i < selectedPlayers.length; i++) {
         // console.log(playerarray[i].playerNames, playerarray[i].playerSkill);
         const name = playerarray[i].playerNames;
@@ -35,8 +34,6 @@ function displayPlayer(selectedPlayers) {
         tablelist.appendChild(tr);
 
     }
-
-
 }
 
 
@@ -49,40 +46,17 @@ function playerName(element) {
         playerNames: playerNames,
         playerSkill: playerSkill
     }
-    playerarray.push(playerobj);
-    // console.log(playerarray);
-    // console.log(playerarray.length);
-    // document.getElementById('total-plyer').innerText = playerarray.length;
-    document.getElementById('total-plyer').innerText = playerarray.length;
-    element.disabled = true;
+    if (playerarray.length >= 5) {
+        alert('Max selected players are not allowed');
+        return;
+    } else {
+        playerarray.push(playerobj);
 
-    displayPlayer(playerarray);
+        document.getElementById('total-plyer').innerText = playerarray.length;
+        element.disabled = true;
+
+        displayPlayer(playerarray);
+    }
+
 }
 
-document.getElementById('calculated').addEventListener('click', function () {
-    const playerCostInput = document.getElementById('player-cost');
-    const playerCost = playerCostInput.value;
-
-    const totelPlayer = document.getElementById('total-plyer').innerText;
-
-    const playerExpenses = document.getElementById('player-expenses');
-
-    const cost = playerCost * totelPlayer;
-
-    playerExpenses.innerText = cost;
-})
-// player calculated part end
-
-// Budget part start 
-document.getElementById('total-calculated').addEventListener('click', function () {
-    const managerCostInput = document.getElementById('manager-cost').value;
-    const managerCost = parseInt(managerCostInput);
-    const coachCostInput = document.getElementById('coach-cost').value;
-    const coachCost = parseInt(coachCostInput);
-
-    const playerExpensesInput = document.getElementById('player-expenses').innerText;
-    const playerExpenses = parseInt(playerExpensesInput);
-
-    const totalExpenses = document.getElementById('total-expenses');
-    totalExpenses.innerText = playerExpenses + managerCost + coachCost;
-})
